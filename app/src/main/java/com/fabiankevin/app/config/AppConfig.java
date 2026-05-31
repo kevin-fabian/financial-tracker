87ed6611bf386ed5d486e4205cc037cd56771c4b
kevin.fabian <fbian.kevin@gmail.com>
2026-05-31 16:57:51 +0800

    Implement caching for transaction service
    - Refactor `transactionService` to use `CachedTransactionService` for caching transactions.
    - Create `CachedTransactionService` to handle caching logic for transaction operations.

--- app/src/main/java/com/fabiankevin/app/config/AppConfig.java
+++ app/src/main/java/com/fabiankevin/app/config/AppConfig.java
@@ -1,18 +1,9 @@
 package com.fabiankevin.app.config;
 
-import com.fabiankevin.app.persistence.AccountRepository;
-import com.fabiankevin.app.persistence.CategoryRepository;
-import com.fabiankevin.app.persistence.TransactionRepository;
 import com.fabiankevin.app.services.*;
-import com.fabiankevin.app.services.summaries.CategorySummaryGenerator;
-import com.fabiankevin.app.services.summaries.DailySummaryGenerator;
-import com.fabiankevin.app.services.summaries.MonthlySummaryGenerator;
-import com.fabiankevin.app.services.summaries.YearlySummaryGenerator;
 import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class AppConfig {
@@ -28,17 +19,8 @@ public class AppConfig {
     }
 
     @Bean
-    public TransactionService transactionService(AccountRepository accountRepository,
-                                                 CategoryRepository categoryRepository,
-                                                 TransactionRepository transactionRepository,
-                                                 CategorySummaryGenerator categorySummaryGenerator,
-                                                 YearlySummaryGenerator yearlySummaryGenerator,
-                                                 MonthlySummaryGenerator monthlySummaryGenerator,
-                                                 DailySummaryGenerator dailySummaryGenerator) {
-        return new DefaultTransactionService(accountRepository, categoryRepository, transactionRepository,
-                List.of(categorySummaryGenerator,
-                        yearlySummaryGenerator,
-                        monthlySummaryGenerator,
-                        dailySummaryGenerator));
+    public TransactionService transactionService(CacheManager cacheManager,
+                                                 DefaultTransactionService delegate) {
+        return new CachedTransactionService(cacheManager, delegate);
     }
 }
