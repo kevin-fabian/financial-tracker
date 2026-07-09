f40aa507d5c945a93a6a36ea1ac9a33c3e12d256
kevin.fabian <fbian.kevin@gmail.com>
2026-07-09 20:19:53 +0800

    Implement user registration functionality
    - Add `DefaultUserClient` to handle user creation via API
    - Create `DefaultUserRegistrationServiceTest` for user registration tests
    - Include tests for successful registration and exception handling

---
+++ app/src/main/java/com/fabiankevin/app/clients/DefaultUserClient.java
@@ -0,0 +1,28 @@
+package com.fabiankevin.app.clients;
+
+import com.fabiankevin.app.clients.dtos.CreateUserRequest;
+import com.fabiankevin.app.clients.dtos.UserResponse;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Component
+public class DefaultUserClient implements UserClient {
+    private final RestClient restClient;
+
+    public DefaultUserClient(RestClient restClient) {
+        this.restClient = restClient.mutate()
+                .baseUrl("http://localhost:9000/api")
+                .build();
+    }
+
+    @Override
+    public UserResponse createUser(CreateUserRequest request) {
+        return restClient.post()
+                .uri("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(request)
+                .retrieve()
+                .body(UserResponse.class);
+    }
+}
