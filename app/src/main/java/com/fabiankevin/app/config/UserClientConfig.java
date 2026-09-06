7a8073ca7631b9952e8359becd01af890bebbb4d
kevin.fabian <fbian.kevin@gmail.com>
2026-09-06 11:37:46 +0800

    Refactor OpenAPI configuration and enhance security settings
    - Remove unused OAuth2 security scheme from OpenApiConfig
    - Add multiple GroupedOpenApi beans for different API groups
    - Update application YAML files to configure OAuth2 resource server and client settings
    - Introduce new application-nonprod.yaml for non-production environment

--- app/src/main/java/com/fabiankevin/app/config/UserClientConfig.java
+++ app/src/main/java/com/fabiankevin/app/config/UserClientConfig.java
@@ -2,6 +2,7 @@ package com.fabiankevin.app.config;
 
 import com.fabiankevin.app.clients.DefaultUserClient;
 import com.fabiankevin.app.clients.UserClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -10,9 +11,9 @@ import org.springframework.web.client.RestClient;
 public class UserClientConfig {
 
     @Bean
-    UserClient userClient(RestClient restClient) {
-        return new DefaultUserClient(restClient,
-                "http://localhost:9000/api",
-                "financial-tracker-client");
+    UserClient userClient(RestClient restClient,
+                          @Value("${app.clients.identity-service.base-url}") String baseUrl,
+                          @Value("${app.clients.identity-service.client-id}") String clientId) {
+        return new DefaultUserClient(restClient, baseUrl, clientId);
     }
 }
