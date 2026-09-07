656af1ad95092127559db85ad95ecb5772d19c9b
kevin.fabian <fbian.kevin@gmail.com>
2026-09-07 09:42:55 +0800

    Refactor schema creation script for financial_tracker_db
    - Change connection to use postgres superuser for schema creation
    - Ensure schema creation and user permissions are correctly set

--- docker-compose/db/init/02-create-schema.sh
+++ docker-compose/db/init/02-create-schema.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -e
 
-# Connect as schema_owner and create the schema
-psql -v ON_ERROR_STOP=1 --username financial_tracker_user --dbname financial_tracker_db <<-EOSQL
-    \connect financial_tracker_db financial_tracker_user
+# Connect as postgres superuser to create the schema in financial_tracker_db
+psql -v ON_ERROR_STOP=1 --username postgres --dbname postgres <<-EOSQL
+-- Connect to financial_tracker_db and create the schema
+\connect financial_tracker_db financial_tracker_user
 
-  CREATE SCHEMA if not exists financial_tracker_schema AUTHORIZATION financial_tracker_user;
-  ALTER USER financial_tracker_user SET search_path to 'financial_tracker_schema';
-  GRANT ALL ON SCHEMA "financial_tracker_schema" TO financial_tracker_user;
+CREATE SCHEMA if not exists financial_tracker_schema AUTHORIZATION financial_tracker_user;
+ALTER USER financial_tracker_user SET search_path to 'financial_tracker_schema';
+GRANT ALL ON SCHEMA "financial_tracker_schema" TO financial_tracker_user;
 GRANT ALL ON SCHEMA "financial_tracker_schema" TO financial_tracker_apps;
 EOSQL
