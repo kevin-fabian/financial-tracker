df4d9d72ca6dc4dbbaeacd18507174ba222aa31a
kevin.fabian <fbian.kevin@gmail.com>
2026-09-14 18:34:16 +0800

    Rename WebSocket endpoint for clarity
    - Change endpoint from "/households" to "ws"

--- app/src/main/java/com/fabiankevin/app/config/websocket/WebSocketConfiguration.java
+++ app/src/main/java/com/fabiankevin/app/config/websocket/WebSocketConfiguration.java
@@ -16,7 +16,7 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/households");
+		registry.addEndpoint("ws");
 	}
 
 	@Override
